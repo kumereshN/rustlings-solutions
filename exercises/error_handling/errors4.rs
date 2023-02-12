d91309f92999b777fd9d4087580abf3f23e5e819
kumereshN <kumeresh6@hotmail.com.com>
2023-02-12 18:02:49 +0800

    stopped at errors4

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -15,7 +15,11 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        match PositiveNonzeroInteger(value as u64) {
+            PositiveNonzeroInteger.value  => {
+                Ok(value)
+            }
+        }
     }
 }
 
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -30,20 +28,33 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                },
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                },
+                Command::Append(no) => {
+                    let bar_str ="bar".repeat(*no);
+                    let mut new_str = string.to_string();
+                    new_str.push_str(&bar_str);
+                    output.push(new_str)
+                },
+            }
         }
         output
     }
 }
-
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
